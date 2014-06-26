a9b6020f413eed2b530385ee27fa62c0f77ea3aa
Antoine Mazelin <antoine.mazelin@free.fr>
2014-06-26 14:18:41 +0200

    finish

--- models/budgetService.py
+++ models/budgetService.py
@@ -18,3 +18,21 @@ class budgetService(object):
           d = datetime.strptime(strDate, '%Y-%m-%d')
           day_string = d.strftime('%d/%m/%Y')
           return day_string
+
+    @staticmethod
+    def getTotalProduitsByBudgetId(unid):
+        query = db(db.achat.budget_id==unid).select()
+        prixTot = 0.0
+    
+        for row in query:
+            prixTot = prixTot+row.Total
+        return prixTot
+
+    @staticmethod
+    def getPercentValue(value, chiffres):
+         return round(value, chiffres)
+
+    @staticmethod
+    def getLastBudgets():
+        newBudgets = db.executesql('SELECT * FROM budget b1 WHERE date_budget = (SELECT max(date_budget) FROM budget b2 WHERE b1.entite_id = b2.entite_id);', as_dict=True)
+        return newBudgets
